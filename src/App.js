31e2dac24de157a923891f7a0750221fc917ec32
angelilton <epifanio.rds@gmail.com>
2021-02-06 21:37:45 +0000

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react'
+import useFetch from './hook/useFetch'
+import styled, { css } from 'styled-components'
+import { ReactComponent as UsdFlag } from './img/usd.svg'
+import { ReactComponent as EurFlag } from './img/eur.svg'
+import { ReactComponent as GbpFlag } from './img/bgp.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import './styles.css'
+
+const App = () => {
+  const { data } = useFetch('currentprice.json')
+  const [checked, setChecked] = useState('USD')
+
+  const handleOnChange = useCallback((e) => {
+    setChecked(e.target.value)
+  }, [])
+
+  console.log(data)
+  return <h1>teste</h1>
+
+  // return (
+  //   <form>
+  //     <Wrapper>
+  //       <Label selected={checked === 'EUR'}>
+  //         <EurFlag />
+  //         cotação atual:
+  //         <b>{data[0]}</b>
+  //         <input
+  //           type="checkbox"
+  //           name="EUR"
+  //           value="EUR"
+  //           checked={checked === 'EUR'}
+  //           onChange={handleOnChange}
+  //         />
+  //       </Label>
+
+  //       <Label selected={checked === 'USD'}>
+  //         <UsdFlag />
+  //         cotação atual:
+  //         <b>{data[1]}</b>
+  //         <input
+  //           type="checkbox"
+  //           name="USD"
+  //           value="USD"
+  //           checked={checked === 'USD'}
+  //           onChange={handleOnChange}
+  //         />
+  //       </Label>
+
+  //       <Label selected={checked === 'GBP'}>
+  //         <GbpFlag />
+  //         cotação atual:
+  //         <b>{data[2]}</b>
+  //         <input
+  //           type="checkbox"
+  //           name="GBP"
+  //           value="GBP"
+  //           checked={checked === 'GBP'}
+  //           onChange={handleOnChange}
+  //         />
+  //       </Label>
+  //     </Wrapper>
+  //   </form>
+  // )
 }
 
-export default App;
+const Label = styled.label`
+  ${({ selected }) => css`
+    border: 1px solid;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    min-width: 155px;
+    margin: 4px;
+    cursor: pointer;
+    ${!!selected && { background: '#4BD295' }}
+  `}
+
+  & > input {
+    opacity: 0;
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 150px;
+  width: 500px;
+  margin: auto;
+`
+
+export default App
---
+++ src/hook/useFetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const url = `https://api.coindesk.com/v1/bpi/`
+
+const useFetch = (search) => {
+  const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  // is fetch data if tag value
+  const fetchData = async (search) => {
+    setIsLoading(true)
+    setIsError(false)
+
+    try {
+      const resp = await axios(`${url}${search}`)
+      const { EUR, USD, GBP } = resp.data.bpi
+
+      setData([EUR.rate, USD.rate, GBP.rate])
+    } catch (error) {
+      setIsError(true)
+    }
+
+    setIsLoading(false)
+  }
+
+  // render when usefetch is require
+  useEffect(() => {
+    fetchData(search)
+  }, [search])
+
+  return { data, setData, fetchData, isLoading, isError }
+}
+
+export default useFetch
