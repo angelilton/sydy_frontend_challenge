4f514b1d3f0e04e9e5482be6cf5f2f2e818e5dbf
angelilton <epifanio.rds@gmail.com>
2021-02-09 13:34:07 +0000

    add margin to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Dashboard from './components/Dashboard'
 import HistoryChart from 'components/HistoryChart'
+import styled from 'styled-components'
 
 const App = () => (
-  <>
+  <Container>
     <Dashboard />
     <HistoryChart />
-  </>
+  </Container>
 )
+
+const Container = styled.section`
+  margin-top: 25px;
+`
 
 export default App
